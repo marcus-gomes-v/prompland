20c75841438eb0a04c21ceef19f4e0211fa9a918
M.V. Bego <viniciusgomes@users.noreply.github.com>
2023-04-06 12:01:19 +0200

    chore: remove old files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
 import Head from 'next/head';
-import AnimationCanvas from '../components/animation/AnimationCanvas';
-import RotatingCube from '../components/animation/RotatingCube';
 import Layout from '../components/base/layout';
 import FooterHome from '../components/footer/footer-home'
 import NavigationHome from '../components/navbar/navigation-home'
-import Faq from '../components/pages/home/faq'
-import Features from '../components/pages/home/features'
 import Hero from '../components/pages/home/hero'
-import LogoCloud from '../components/pages/home/logo-cloud'
-import Product from '../components/pages/home/product';
 import Testimonials from '../components/pages/home/testimonials'
-import AnimationCanvasCore from '../components/animation/AnimationCanvasCore';
 import AnimationCanvasBall from '../components/animation/AnimationCanvasBall';
 
 export default function Home() {
