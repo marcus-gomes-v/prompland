4299fc5683b7aefe0af4e7b9abc1785e30f50692
M.V. Bego <viniciusgomes@users.noreply.github.com>
2023-04-06 17:41:58 +0200

    chore: add sitemap

--- pages/api/sitemap.ts
+++ pages/api/sitemap.ts
@@ -10,11 +10,19 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
   // generate sitemap here
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
-    <url>
-      <loc>http://www.example.com/foo.html</loc>
-      <lastmod>2021-01-01</lastmod>
-    </url>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>https://www.prompland.com/</loc>
+        <lastmod>2023-04-06</lastmod>
+      </url>
+      <url>
+        <loc>https://www.prompland.com/authentication</loc>
+        <lastmod>2023-04-06</lastmod>
+      </url>
+      <url>
+        <loc>https://www.prompland.com/sign_up</loc>
+        <lastmod>2023-04-06</lastmod>
+      </url>
     </urlset>`
 
   res.end(xml)
